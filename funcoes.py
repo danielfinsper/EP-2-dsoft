c1363e4543acb61668e948e2eea55c20785746ff
danielfinsper <danielgf2@al.insper.edu.br>
2025-04-30 15:06:46 -0300

    Q4

--- funcoes.py
+++ funcoes.py
@@ -13,3 +13,33 @@ def remover_dado (dados_rolados,dados_no_estoque,dado_removido):
     del dados_no_estoque[dado_removido]
     lista = [dados_rolados,dados_no_estoque]
     return (lista)
+
+def calcula_pontos_regra_simples(face_dos_dados):
+    dvalores={}
+    v1=0
+    v2=0
+    v3=0
+    v4=0
+    v5=0
+    v6=0
+    for n in face_dos_dados:
+        if n == 1:
+            v1= v1 + 1
+        if n == 2:
+            v2+= 2
+        if n == 3:
+            v3+=3
+        if n == 4:
+            v4+=4
+        if n == 5:
+            v5+=5
+        if n ==6:
+            v6+=6
+    dvalores[1]=v1
+    dvalores[2]=v2
+    dvalores[3]=v3
+    dvalores[4]=v4
+    dvalores[5]=v5
+    dvalores[6]=v6
+    return dvalores
+print(calcula_pontos_regra_simples([2, 3, 4, 5, 2]))
